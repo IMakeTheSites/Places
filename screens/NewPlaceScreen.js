b76f5db7f692a0909162a5a66961d9220f175772
Mark Wlodawski <imakethesites@gmail.com>
2021-02-08 17:40:23 -0500

    Storing the address and Updating location screen

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { useDispatch } from "react-redux";
@@ -10,7 +10,7 @@ import LocationPicker from "../components/LocationPicker";
 const NewPlaceScreen = (props) => {
   const [titleValue, setTitleValue] = useState("");
   const [selectedImage, setSelectedImage] = useState();
-
+  const [selectedLocation, setSelectedLocation] = useState();
   const dispatch = useDispatch();
 
   const titleChangeHandler = (text) => {
@@ -21,8 +21,14 @@ const NewPlaceScreen = (props) => {
     setSelectedImage(imagePath);
   };
 
+  const locationPickedHandler = useCallback((location) => {
+    setSelectedLocation(location);
+  }, []);
+
   const savePlaceHandler = () => {
-    dispatch(placesActions.addPlace(titleValue, selectedImage));
+    dispatch(
+      placesActions.addPlace(titleValue, selectedImage, selectedLocation)
+    );
     props.navigation.goBack();
   };
 
@@ -36,7 +42,10 @@ const NewPlaceScreen = (props) => {
           value={titleValue}
         />
         <ImagePicker onImageTaken={imageTakenHandler} />
-        <LocationPicker navigation={props.navigation} />
+        <LocationPicker
+          navigation={props.navigation}
+          onLocationPicked={locationPickedHandler}
+        />
         <Button
           title="Save Place"
           color={Colors.primary}
@@ -68,3 +77,75 @@ const styles = StyleSheet.create({
   },
 });
 export default NewPlaceScreen;
+
+// const NewPlaceScreen = props => {
+//     const [titleValue, setTitleValue] = useState('');
+//     const [selectedImage, setSelectedImage] = useState();
+//     const [selectedLocation, setSelectedLocation] = useState();
+
+//     const dispatch = useDispatch();
+
+//     const titleChangeHandler = text => {
+//       // you could add validation
+//       setTitleValue(text);
+//     };
+
+//     const imageTakenHandler = imagePath => {
+//       setSelectedImage(imagePath);
+//     };
+
+//     const locationPickedHandler = useCallback(location => {
+//       setSelectedLocation(location);
+//     }, []);
+
+//     const savePlaceHandler = () => {
+//       dispatch(placesActions.addPlace(titleValue, selectedImage, selectedLocation));
+//       props.navigation.goBack();
+//     };
+
+//     return (
+//       <ScrollView>
+//         <View style={styles.form}>
+//           <Text style={styles.label}>Title</Text>
+//           <TextInput
+//             style={styles.textInput}
+//             onChangeText={titleChangeHandler}
+//             value={titleValue}
+//           />
+//           <ImagePicker onImageTaken={imageTakenHandler} />
+//           <LocationPicker
+//             navigation={props.navigation}
+//             onLocationPicked={locationPickedHandler}
+//           />
+//           <Button
+//             title="Save Place"
+//             color={Colors.primary}
+//             onPress={savePlaceHandler}
+//           />
+//         </View>
+//       </ScrollView>
+//     );
+//   };
+
+//   NewPlaceScreen.navigationOptions = {
+//     headerTitle: 'Add Place'
+//   };
+
+//   const styles = StyleSheet.create({
+//     form: {
+//       margin: 30
+//     },
+//     label: {
+//       fontSize: 18,
+//       marginBottom: 15
+//     },
+//     textInput: {
+//       borderBottomColor: '#ccc',
+//       borderBottomWidth: 1,
+//       marginBottom: 15,
+//       paddingVertical: 4,
+//       paddingHorizontal: 2
+//     }
+//   });
+
+//   export default NewPlaceScreen;
